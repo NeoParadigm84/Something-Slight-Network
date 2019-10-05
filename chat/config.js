1ef833f90a18530202a66abe01357e1239b472f3
CJ Hubbard <mysticriver84@gmail.com>
2019-10-05 07:14:54 -0400

    chat update not working fully

---
+++ chat/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b6f1908e-fbfd-4334-a8b8-ea74ae8a5670/token";
+const instanceLocator = "v1:us1:b6f1908e-fbfd-4334-a8b8-ea74ae8a5670";
+
+export { tokenUrl, instanceLocator }
